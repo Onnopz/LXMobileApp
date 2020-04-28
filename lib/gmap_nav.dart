44f6e83429a0c2dc67de09a398c8b8dbd8814d04
Noppanut <noppanut.gapsb2536@mail.kmutt.ac.th>
2020-04-28 21:55:15 +0700

    (เพิ่ม) google map

--- lib/gmap_nav.dart
+++ lib/gmap_nav.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'data_storage.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class GMapNavigator extends StatefulWidget {
   GMapNavigator();
@@ -11,26 +13,30 @@ class GMapNavigator extends StatefulWidget {
 class _GMapNavigatorState extends State<GMapNavigator> {
   static final pageIndex = 2;
 
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: CustomNavigationBar((int i) {
-        if(i != pageIndex){
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => CustomNavigationBar.page[i]));
-        }
-      },
-          pageIndex),
-      backgroundColor: AppConstant.color_Background,
-      body: CustomBodyContainer(
-        <Widget>[
-            CustomText(
-              "Text", 0, 0, 0, 0, 30.0
-            ),
-        ],
-          context
-      ),
+        bottomNavigationBar: CustomNavigationBar((int i) {
+          if (i != pageIndex) {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => CustomNavigationBar.page[i]));
+          }
+        }, pageIndex),
 
-    );
+        backgroundColor: AppConstant.color_Background,
+
+        body: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(13.7650836, 100.5379664),
+            zoom: 16,
+          ),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),  
+        );
   }
 }
